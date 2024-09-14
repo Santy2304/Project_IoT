8998f0ffd49346910e920e6f6e3154709e7b199d
Hineill <hineill.cespedes@pucp.edu.pe>
2024-09-13 23:35:20 -0500

    Combio de vistas

--- app/src/main/java/com/example/project_iot/Login/VistaInicial.java
+++ app/src/main/java/com/example/project_iot/Login/VistaInicial.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.project_iot.R;
+import com.example.project_iot.databinding.ActivityMainBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,7 +23,6 @@ public class VistaInicial extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -61,6 +62,8 @@ public class VistaInicial extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vista_inicial, container, false);
+        View view  = inflater.inflate(R.layout.fragment_vista_inicial, container, false);
+
+        return view;
     }
 }
--- app/src/main/java/com/example/project_iot/LoginActivity.java
+++ app/src/main/java/com/example/project_iot/LoginActivity.java
@@ -1,10 +1,17 @@
 package com.example.project_iot;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.project_iot.Login.InicioSesion;
+import com.example.project_iot.Login.VistaInicial;
 
 public class LoginActivity extends AppCompatActivity {
     //Primera vista del login
@@ -13,6 +20,28 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.vista_inicial);
+        //Debemos asignar primero una vista al fragmentLayout
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragmento_main_login, VistaInicial.class, null)
+                    .commit();
+        }
+    }
+
+    public void saltarInicioSesion(View view){
+        Fragment fragment = getSupportFragmentManager()
+                .findFragmentById(R.id.fragmento_main_login);
+
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left)
+                    .replace(R.id.fragmento_main_login , InicioSesion.class, null)
+                    .commit();
+
+        }
+
+
     }
 
 }
