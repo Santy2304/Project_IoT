fcea3a90c99379548047a53063b64ce3f22800fd
J3anP <a20213733@pucp.edu.pe>
2024-09-17 18:28:31 -0500

    prueba corrección 35%

--- app/src/main/java/com/example/project_iot/SuperAdminActivity.java
+++ app/src/main/java/com/example/project_iot/SuperAdminActivity.java
@@ -19,44 +19,11 @@ public class SuperAdminActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        //setContentView(R.layout.inicio_superadmin);
+        setContentView(R.layout.activity_principal_superadmin);
 
 
-        setContentView(R.layout.sup_admin_fragment_perfil_cliente);
-
-        switchMaterial = findViewById(R.id.switchMaterial);
-        tvEstado = findViewById(R.id.tvEstado);
-
-        switchMaterial.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    tvEstado.setText("Activado");
-                } else {
-                    tvEstado.setText("Desactivado");
-                }
-            }
-        });
     }
 
-
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-        //De acuerdo al escogido se realiza el cambio de vista
-
-        if(item.getItemId() == R.id.principal  ){
-            //En caso de click a esto se suelta un fragmento
-            return true ;
-        }
-        if(item.getItemId() == R.id.restaurant ){
-            //En caso de click a esto se suelta un fragmento
-            return true ;
-        }
-        if(item.getItemId() == R.id.profile ){
-            //En caso de click a esto se suelta un fragmento
-            return true ;
-        }
-        return super.onContextItemSelected(item);
-    }
 
 
 }
