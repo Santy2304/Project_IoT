016656ee8b9a8644874afce63a702f48ac23b809
Hineill <hineill.cespedes@pucp.edu.pe>
2024-09-18 12:24:16 -0500

    repartidor al 50%

--- app/src/main/java/com/example/project_iot/RepartidorActivity.java
+++ app/src/main/java/com/example/project_iot/RepartidorActivity.java
@@ -1,13 +1,20 @@
 package com.example.project_iot;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.example.project_iot.ClienteFragments.ClienteVistaRestaurante;
 import com.example.project_iot.Login.VistaInicial;
+import com.example.project_iot.RepartidorFragments.DetalleCompraDelivery;
+import com.example.project_iot.RepartidorFragments.RepartidorConfirmacionTomarPedido;
 import com.example.project_iot.RepartidorFragments.RepartidorListaPedidosPosibles;
+import com.example.project_iot.RepartidorFragments.RepartidorRechazoTomarPedido;
+import com.example.project_iot.RepartidorFragments.RepartidorVerMapaPedido;
 
 public class RepartidorActivity extends AppCompatActivity {
     @Override
@@ -20,6 +27,53 @@ public class RepartidorActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
                     .replace(R.id.fragmentoPrincipalRepartidor, RepartidorListaPedidosPosibles.class, null)
+                    .commit();
+        }
+    }
+
+
+
+
+    //-------------PARA IR A UNA VISTA----------------------
+    public void verDetalleCompraDelivery(View view){
+        Fragment fragment = getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentoPrincipalRepartidor);
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left)
+                    .replace(R.id.fragmentoPrincipalRepartidor, DetalleCompraDelivery.class, null)
+                    .commit();
+        }
+    }
+    public void verRepartidorVerMapaPedido(View view){
+        Fragment fragment = getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentoPrincipalRepartidor);
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left)
+                    .replace(R.id.fragmentoPrincipalRepartidor, RepartidorVerMapaPedido.class, null)
+                    .commit();
+        }
+    }
+
+    public void verRepartidorConfirmacionTomarPedido(View view){
+        Fragment fragment = getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentoPrincipalRepartidor);
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left)
+                    .replace(R.id.fragmentoPrincipalRepartidor, RepartidorConfirmacionTomarPedido.class, null)
+                    .commit();
+        }
+    }
+
+    public void verRepartidorRechazarTomarPedido(View view){
+        Fragment fragment = getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentoPrincipalRepartidor);
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left)
+                    .replace(R.id.fragmentoPrincipalRepartidor, RepartidorRechazoTomarPedido.class, null)
                     .commit();
         }
     }
