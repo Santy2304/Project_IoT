2c0ca2e5bee88b9db5f4194c68d068676935370e
Hineill <hineill.cespedes@pucp.edu.pe>
2024-09-23 21:38:46 -0500

    Creando carpetas

---
+++ app/src/main/java/com/example/project_iot/LoginActivities/InicioApp.java
@@ -0,0 +1,29 @@
+package com.example.project_iot.LoginActivities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.project_iot.R;
+
+public class InicioApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicio_app);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+
+}
