768eb3900b33ff9238fa611d7e631b6909d052b4
J3anP <a20213733@pucp.edu.pe>
2024-09-25 20:35:43 -0500

    le quite la foto expansible (se ve horrible)

--- app/src/main/java/com/example/project_iot/SuperAdminFragments/PerfilSuperAdmin.java
+++ app/src/main/java/com/example/project_iot/SuperAdminFragments/PerfilSuperAdmin.java
@@ -59,7 +59,7 @@ public class PerfilSuperAdmin extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.sup_admin_fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.sup_admin_perfil, container, false);
 
         return view;
     }
